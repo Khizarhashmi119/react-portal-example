c98569439e6f5aa73b7c5e1f3c4018b14b274a0c
khizarhashmi119 <khizarhashmi119@gmail.com>
2024-03-16 18:04:56 +0530

    Add animation on close btn.

--- src/component/Modal/index.tsx
+++ src/component/Modal/index.tsx
@@ -14,7 +14,7 @@ const Modal = forwardRef<HTMLDivElement | null, Props>((props, ref) => {
       <div ref={ref} className="modal">
         <h1>Modal title</h1>
         <button className="close-btn" type="button" onClick={handleClickClose}>
-          Close
+          <span className="close-btn-text">Close</span>
         </button>
       </div>
     </div>
